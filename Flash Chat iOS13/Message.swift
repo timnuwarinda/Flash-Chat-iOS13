2f1121305efdbfeda8542a66f1f03bff5355b4b3
Timo Niwarinda <timoniwarinda@Nduwayos-MacBook-Pro-2.local>
2019-12-13 01:19:14 -0600

    all changes in effect

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Timo Niwarinda on 12/11/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct Message {
+    let sender: String
+    let body: String
+}
